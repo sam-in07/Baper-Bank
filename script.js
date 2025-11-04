c73191b85a11d486d547a955040a6bd120214bd4
Samin Ahmed Chowdhury <85004796+sam-in07@users.noreply.github.com>
2025-11-04 06:43:03 +0600

    scripts js done

--- script.js
+++ script.js
@@ -1,2 +1,42 @@
-console.log('javascript connected');
+console.log("javascript connected");
 
+// button functionality
+document
+  .getElementById("btn-login")
+  .addEventListener("click", function (event) {
+    event.preventDefault();
+    console.log("button clicked");
+    // console.log(event);
+
+    const mobileNumber = "01991949385";
+    const pinNumber = "9876";
+
+    // console.log(mobileNumber);
+
+    const inputNumber = document.getElementById("mobile-number-input");
+    // console.log(inputNumber.value);
+    const inputPin = document.getElementById("pin-number-input");
+    // console.log(inputPin.value);
+
+    // const inputNumberValue = inputNumber.value;
+    // const inputPinValue = inputPin.value;
+    const inputNumberValue = document.getElementById(
+      "mobile-number-input"
+    ).value;
+    const inputPinValue = document.getElementById("pin-number-input").value;
+
+    const inputNumberConverted = parseInt(inputNumberValue);
+    // console.log(inputNumberConverted);
+    const inputPinValueConverted = parseInt(inputPinValue);
+    // console.log(inputPinValueConverted);
+
+    if (
+      inputNumberConverted === mobileNumber &&
+      inputPinValueConverted === pinNumber
+    ) {
+      window.location.href = "./home-page.html";
+    } else {
+      // console.log('wrong credentials');
+      alert("Invalid Credentials");
+    }
+  });
